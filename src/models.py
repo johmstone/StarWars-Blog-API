1a5427aeed58da9ee8146fbbf463bcc9e3253dbd
johmstone <johmstone@gmail.com>
2021-03-30 15:56:06 +0000

    Initial commit

--- src/models.py
+++ src/models.py
@@ -2,11 +2,29 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     password = db.Column(db.String(80), unique=False, nullable=False)
+#     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+#     def __repr__(self):
+#         return '<User %r>' % self.username
+
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "email": self.email,
+#             # do not serialize the password, its a security breach
+#         }
+
 class User(db.Model):
+    # Notice that each column is also a normal Python instance attribute.
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    username = db.Column(db.String(250), unique=True, nullable=False)
+    fullname = db.Column(db.String(250), unique=False, nullable=False)
+    lastname = db.Column(db.String(250), unique=False, nullable=False)
+    password = db.Column(db.String(250), unique=False, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -14,6 +32,102 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "username": self.username,
+            "fullname": self.fullname,
+            "lastname": self.lastname,
+            # do not serialize the password, its a security breach
+        }
+
+class Character(db.Model):
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    description = db.Column(db.String)
+    height = db.Column(db.Integer)
+    hair_color = db.Column(db.String(50))
+    skin_color = db.Column(db.String(50))
+    eye_color = db.Column(db.String(50))
+    birth_year = db.Column(db.String(50))
+    gender = db.Column(db.String(25))
+
+    def __repr__(self):
+        return '<Character %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "height": self.height,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            # do not serialize the password, its a security breach
+        }
+
+class Planet(db.Model):
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    description = db.Column(db.String)
+    diameter = db.Column(db.Integer)
+    rotation_period = db.Column(db.Integer)
+    orbital_period = db.Column(db.Integer)
+    gravity = db.Column(db.String(50))
+    population = db.Column(db.Integer)
+    climate = db.Column(db.String(25))
+    terrain = db.Column(db.String(25))
+    surface_water = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Planet %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "diameter": self.diameter,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "gravity": self.gravity,
+            "population": self.population,
+            "climate": self.climate,
+            "terrain": self.terrain,
+            "surface_water": self.surface_water,
+            # do not serialize the password, its a security breach
+        }
+
+class CharacterFavorite(db.Model):
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    userid = db.Column(db.Integer, db.ForeignKey('User.id'))
+    characterid = db.Column(db.Integer, db.ForeignKey('Character.id'))
+    users = db.relationship(User)
+    characters = db.relationship(Character)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "userid": self.userid,
+            "characterid": self.characterid,            
+            # do not serialize the password, its a security breach
+        }
+
+class PlanetFavorite(db.Model):
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    planetid = db.Column(db.Integer, db.ForeignKey('Planet.id'))
+    userid = db.Column(db.Integer, db.ForeignKey('User.id'))
+    planets = db.relationship(Planet)
+    users = db.relationship(User)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "userid": self.userid,
+            "planetid": self.planetid,            
             # do not serialize the password, its a security breach
         }
